5fb9bcfd123bd142c281f00d4e4fdd3b4c82618a
jacky917 <psnjacky917@gmail.com>
2023-01-27 08:04:47 +0900

    剑指 Offer 09. 用两个栈实现队列

--- src/main/java/l09/l09_0127.java
+++ src/main/java/l09/l09_0127.java
@@ -1,17 +1,40 @@
 package l09;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class l09_0127 {
+
+    Queue<Integer> append = new ArrayDeque<>();
+    Queue<Integer> delete = new ArrayDeque<>();
 
     public l09_0127() {
 
     }
 
     public void appendTail(int value) {
-        System.out.println("Test JUnit" + value);
+        append.add(value);
     }
 
     public int deleteHead() {
-        return 0;
+        // 待刪除棧有值，直接取出頂層元素返回
+        if (delete.size() > 0) return delete.poll();
+        // 待刪除棧無值，但新加入棧有值
+        if (append.size() > 0) {
+            int size = append.size();
+            for (int i = 0; i < size; i++) {
+                delete.add(append.poll());
+            }
+            return delete.poll();
+        }
+        // 都無值，刪除失敗
+        return -1;
     }
 
+    public void print() {
+        System.out.println("----------------");
+        System.out.println("append : " + append);
+        System.out.println("delete : " + delete);
+        System.out.println("----------------");
+    }
 }
--- src/test/java/JUnit5TestClass.java
+++ src/test/java/JUnit5TestClass.java
@@ -10,6 +10,14 @@ public class JUnit5TestClass {
     @Test
     public void l09(){
         l09_0127 l09_0127 = new l09_0127();
-        l09_0127.appendTail(10);
+        System.out.println("deleteHead : " + l09_0127.deleteHead());
+        l09_0127.appendTail(1);
+        l09_0127.appendTail(2);
+        l09_0127.appendTail(3);
+        l09_0127.appendTail(4);
+        l09_0127.print();
+        System.out.println("deleteHead : " + l09_0127.deleteHead());
+        System.out.println("deleteHead : " + l09_0127.deleteHead());
+        l09_0127.print();
     }
 }
